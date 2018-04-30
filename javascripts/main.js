8a0acd70d3b76c95c400519571ef499dc24b33bd
Matthew Rennie <matthew@jemstep.com>
2018-04-30 14:09:56 -0700

    updated acr values

--- javascripts/main.js
+++ javascripts/main.js
@@ -15,7 +15,7 @@ var settings = {
     redirect_uri: window.location.protocol + "//" + window.location.host + window.location.pathname,
     response_type: 'id_token token',
     scope: 'openid profile email',
-    acr_values: 'basic',
+    acr_values: 'auth_ldap_server',
     filterProtocolClaims: true,
     loadUserInfo: true
 };
